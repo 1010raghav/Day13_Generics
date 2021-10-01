20c361f03f4e17b5de6cf9ff795c5468b34726b4
Raghav <raghavwandre1234@gmail.com>
2021-10-01 15:26:56 +0530

    UC1- Compare numbers int float string

---
+++ Day13_practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day13_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Value");
+
+            int output = UC1.Maxint(11,22,33);
+            Console.WriteLine(output);
+
+           double doubleoutput = UC1.Maxint(11.2, 22.1, 33.23);
+            Console.WriteLine(doubleoutput);
+
+            string stringoutput = UC1.Maxint("22", "33", "44");
+            Console.WriteLine(stringoutput);
+
+
+        }
+    }
+}
---
+++ Day13_practice/UC1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_practice
+{
+    class UC1
+    {
+        public static int Maxint(int First, int second, int third)
+        {
+            if (First.CompareTo(second)>0 && First.CompareTo(third) > 0 )
+            {
+                return First;
+            }
+
+            if (second.CompareTo(First) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+
+            if (third.CompareTo(second) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+
+            throw new Exception("First Number , Second Number , Third Number are Same");
+        }
+
+        public static double Maxint(double First,double second, double third)
+        {
+            if (First.CompareTo(second) > 0 && First.CompareTo(third) > 0)
+            {
+                return First;
+            }
+
+            if (second.CompareTo(First) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+
+            if (third.CompareTo(second) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+
+            throw new Exception("First Number , Second Number , Third Number are Same");
+        }
+
+        public static string Maxint(string First, string second, string third)
+        {
+            if (First.CompareTo(second)>0 && First.CompareTo(third)>0 ||
+                First.CompareTo(second) >= 0 && First.CompareTo(third) > 0 ||
+                First.CompareTo(second) > 0 && First.CompareTo(third) >= 0)
+            {
+                return First;
+            }
+
+            if (second.CompareTo(First) > 0 && second.CompareTo(third) > 0 ||
+                second.CompareTo(First) >= 0 && second.CompareTo(third) > 0 ||
+                second.CompareTo(First) > 0 && second.CompareTo(third) >= 0)
+            {
+                return second;
+            }
+
+            if (third.CompareTo(second) > 0 && third.CompareTo(second) > 0 ||
+                third.CompareTo(second) >= 0 && third.CompareTo(second) > 0 ||
+                third.CompareTo(second) > 0 && third.CompareTo(second) >= 0)
+            {
+                return third;
+            }
+            throw new Exception("First Number , Second Number , Third Number are Same");
+
+        }
+
+    }
+
+
+}
